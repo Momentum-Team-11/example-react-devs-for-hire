ca658a54e59b87ed05d913f99f1ac3b8699874e9
Amy Gori <amydgori@gmail.com>
2022-03-29 12:31:07 -0400

    Select one dev and only show one if selected

--- src/App.js
+++ src/App.js
@@ -4,12 +4,12 @@ import Developer from './components/Developer'
 import { developerData } from './developerData'
 
 const App = () => {
+  const developers = developerData
   const [selectedDev, setSelectedDev] = useState(null)
 
   return (
     <>
       <h1>Devs for Hire 👩‍💻</h1>
-      // show a list of devs
       {selectedDev ? (
         <Developer
           name={selectedDev.name}
@@ -17,15 +17,17 @@ const App = () => {
           github={selectedDev.gitHubUsername}
         />
       ) : (
-        developerData.map((dev, idx) => {
+        developers.map((dev, idx) => {
           if (dev.availableForHire) {
             return (
-              <Developer
-                name={dev.name}
-                skills={dev.skills}
-                key={idx}
-                github={dev.gitHubUsername}
-              />
+              <div className="dev-card" onClick={() => setSelectedDev(dev)}>
+                <Developer
+                  name={dev.name}
+                  skills={dev.skills}
+                  key={idx}
+                  github={dev.gitHubUsername}
+                />
+              </div>
             )
           }
         })
