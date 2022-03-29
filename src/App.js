a67ec730d9cb04cab6dfc091b315fc34d717a3bd
Amy Gori <amydgori@gmail.com>
2022-03-29 13:08:08 -0400

    Remove unused imports

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
-import axios from 'axios'
-import { useEffect, useState } from 'react'
+import { useState } from 'react'
 import Developer from './components/Developer'
 import { developerData } from './developerData'
 
